e4d889801b0dc2656a644614325bbfbb8e70d4a9
X_Worm <vasilkindiy@gmail.com>
2019-03-20 22:24:53 +0200

    All_Task_Added

---
+++ Fanno/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Laba1_TheoryOfInfirmation_V_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadMessage = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EntropiaBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InfoBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.EffectiveLengthBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LoadMessage
+            // 
+            this.LoadMessage.Location = new System.Drawing.Point(13, 17);
+            this.LoadMessage.Name = "LoadMessage";
+            this.LoadMessage.Size = new System.Drawing.Size(238, 81);
+            this.LoadMessage.TabIndex = 0;
+            this.LoadMessage.Text = "Завантажити Повідомлення";
+            this.LoadMessage.UseVisualStyleBackColor = true;
+            this.LoadMessage.Click += new System.EventHandler(this.LoadMessage_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 164);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(489, 215);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label1.Location = new System.Drawing.Point(266, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ентропія джерела повідомлення";
+            // 
+            // EntropiaBox
+            // 
+            this.EntropiaBox.Enabled = false;
+            this.EntropiaBox.Location = new System.Drawing.Point(269, 34);
+            this.EntropiaBox.Name = "EntropiaBox";
+            this.EntropiaBox.Size = new System.Drawing.Size(233, 20);
+            this.EntropiaBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label2.Location = new System.Drawing.Point(312, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Кількість інформації";
+            // 
+            // InfoBox
+            // 
+            this.InfoBox.Enabled = false;
+            this.InfoBox.Location = new System.Drawing.Point(269, 78);
+            this.InfoBox.Name = "InfoBox";
+            this.InfoBox.Size = new System.Drawing.Size(233, 20);
+            this.InfoBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label3.Location = new System.Drawing.Point(157, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Кодування Шеннона-Фано";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.EffectiveLengthBox);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(13, 386);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(489, 76);
+            this.panel1.TabIndex = 7;
+            // 
+            // EffectiveLengthBox
+            // 
+            this.EffectiveLengthBox.Enabled = false;
+            this.EffectiveLengthBox.Location = new System.Drawing.Point(106, 8);
+            this.EffectiveLengthBox.Name = "EffectiveLengthBox";
+            this.EffectiveLengthBox.Size = new System.Drawing.Size(100, 20);
+            this.EffectiveLengthBox.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "L.сер.еф";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "НижняОцінка";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(106, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 474);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.InfoBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EntropiaBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.LoadMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadMessage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EntropiaBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InfoBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox EffectiveLengthBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Fanno/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+
+namespace Laba1_TheoryOfInfirmation_V_8
+{
+    public partial class Form1 : Form
+    {
+        
+        private OpenFileDialog openFileDialog1;
+        public string FileName , TextMessage;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadMessage_Click(object sender, EventArgs e) // Загрузка повідомлення та проведення обчислень
+        {
+            openFileDialog1 = new OpenFileDialog(); 
+
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FileName = openFileDialog1.FileName;
+                MessageBox.Show(FileName);
+
+                StreamReader strReader = new StreamReader(FileName , Encoding.GetEncoding(1251)); // Відкриття потоку
+                TextMessage =  strReader.ReadToEnd(); // Зчитування текстового файлу
+               List<NodeSymbol> symbols = FillListBox(TextMessage); // контейнер для збереження повідомлень та іншої інформації
+               double Entropia = CalculateEntropia(symbols); // Знаходження ентропії повідомлень
+                InfoBox.Text = (Entropia*symbols.Count).ToString(); // Виведення ентропії
+
+            
+                SortSymbols(ref symbols); // Сортування повідомлень за спаданням ймовірності 
+                Fano(0, symbols.Count -1, ref symbols); // алгоритм кодування за Шеноном Фано
+                for (int i = 0; i < symbols.Count; i++)
+                {
+                    symbols[i].SetFano();
+                }
+
+                double EffectiveLength = calculateEffectiveLength(symbols); // визначення середньої кількості символів на повідомлення
+                EffectiveLengthBox.Text = EffectiveLength.ToString(); // виведення значення L.ефек.сер
+
+                textBox1.Text = Entropia.ToString();
+
+
+                strReader.Close(); // Закриття потоку
+            }
+        }
+
+        private void SortSymbols(ref List<NodeSymbol> symbols) // Сортування символів в порядку спадання ймовірності
+        {
+            string tempSymbol = "";
+            int tempCounter = 0;
+            double tempProbability = 0.0;
+            for(int i = 0; i < symbols.Count; i++)
+            {
+                for(int j = 0; j < symbols.Count-1; j++)
+                {
+                    if(symbols[j].Probability < symbols[j + 1].Probability)
+                    {
+                        tempSymbol = symbols[j].Symbol;
+                        tempCounter = symbols[j].CounterShow;
+                        tempProbability = symbols[j].Probability;
+
+                        symbols[j].Probability = symbols[j +1].Probability;
+                        symbols[j].Symbol = symbols[j+1].Symbol;
+                        symbols[j].CounterShow = symbols[j+1 ].CounterShow;
+
+                        symbols[j + 1].Probability = tempProbability;
+                        symbols[j + 1].CounterShow = tempCounter;
+                        symbols[j + 1].Symbol = tempSymbol;
+                    }
+                }
+            }
+        }
+
+        public double calculateEffectiveLength(List<NodeSymbol> symbols) // Знаходження значення L.сер.еф
+        {
+            double result = 0;
+            for(int i = 0; i < symbols.Count; i++)
+            {
+                result += symbols[i].Probability * symbols[i].Fano.Count();
+            }
+            return result;
+        }
+
+        private List<NodeSymbol> FillListBox(string TextMessage) // Заповнення таблиці значень та підрахунок ймовірності зустрічання повідомлень
+        {
+            int SymbolLength = TextMessage.Length;
+            List<NodeSymbol> symbols = new List<NodeSymbol>();
+            List<char> UniqueSymbol = new List<char>();
+            for(int i = 0; i < TextMessage.Length; i++)
+            {
+                
+                char temp = TextMessage[i];
+                if (temp == '\r' || temp == '\n') { SymbolLength--; continue; }
+                if (UniqueSymbol.Contains(temp))
+                {
+                    SeekSymbol(ref symbols, temp);
+                }
+                else { symbols.Add(new NodeSymbol(temp, 0, 0)); UniqueSymbol.Add(temp); }
+            }
+            CalculateProbability(ref symbols, SymbolLength);
+
+            var bindinglist = new BindingList<NodeSymbol>(symbols);
+            var source = new BindingSource(bindinglist, null);
+            dataGridView1.DataSource = source;
+            return symbols;
+        }
+        private void CalculateProbability(ref List<NodeSymbol> symbols , int symbolLength) // Підрахунок ймовірностей повідомлень
+        {
+            for(int i = 0; i < symbols.Count; i++)
+            {
+                symbols[i].Probability = Math.Round((double)symbols[i].Counter / symbolLength , 3);
+            }
+        }
+      
+        public double CalculateEntropia(List<NodeSymbol> symbols) // Підрахунок ентропії
+        {
+            double result = 0;
+            for(int i = 0; i < symbols.Count; i++)
+            {
+                result += symbols[i].Probability * Math.Log(symbols[i].Probability, 2);
+            }
+            result = Math.Abs(result);
+            result = Math.Round(result, 3);
+            var ForEntropiaBox = result.ToString();
+            EntropiaBox.Text = ForEntropiaBox;
+            return result;
+        }
+
+        private void SeekSymbol( ref List<NodeSymbol> symbols , char temp)
+        {
+            for(int i = 0; i < symbols.Count; i++)
+            {
+                if(symbols[i].Symbol == temp.ToString())
+                {
+                    symbols[i].Counter++;
+                    symbols[i].CounterShow++;
+                    break;
+                }
+            }
+        }
+
+
+        double count1 = 0, count2 = 0; int m; int g = 0;
+
+        public int DividePoslidovnist(int L , int R , ref List<NodeSymbol> symbols) // Розділення послідовності символів на рівноймовірні частини
+        {
+            count1 = 0;
+            for(int i = L; i <= R-1; i++)
+            {
+                count1 += symbols[i].Probability;
+            }
+
+            count2 = symbols[R].Probability;
+            m = R;
+            while(count1 >= count2)
+            {
+                m = m - 1;
+                count1 -= symbols[m].Probability;
+                count2 += symbols[m].Probability;
+            }
+            return m;
+
+        }
+
+        public void Fano(int L , int R , ref List<NodeSymbol> symbols) // Алгоритм Шеннона Фано
+        {
+            int n;
+
+            if(L < R)
+            {
+                n = DividePoslidovnist(L, R, ref symbols);
+                for(int i = L; i<= R; i++)
+                {
+                    if(i <= n)
+                    {
+                        symbols[i].Res  += Convert.ToByte(1);
+                    }
+                    else
+                    {
+                        symbols[i].Res += Convert.ToByte(0);
+                    }
+                }
+                Fano(L, n , ref symbols);
+                Fano(n + 1, R, ref symbols);
+            }
+        }
+
+       
+        }
+    }
+
+    public class NodeSymbol // Клас для збереження символу, ймовірності, коду
+    {
+        public string Symbol { get; set; }
+        public int Counter = 0;
+        public double Probability { get; set; }
+        public int CounterShow { get; set; }
+    public string Res = "";
+    public string Fano { get; set; }
+        public NodeSymbol(char symbol , int counter , int probability)
+        {
+            Symbol = symbol.ToString();
+            Counter = 1;
+            CounterShow = Counter;
+            Probability = probability;
+        }
+    public void SetFano()
+    {
+        Fano = Res;
+    }
+    }
+
+   
+
+
